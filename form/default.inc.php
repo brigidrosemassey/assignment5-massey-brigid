d1976ce5fa50eb10e4fb479f8bbdd017729aa80d
Brigid <bigidm>
2021-04-07 13:56:09 -0400

    downloaded free php submit

---
+++ form/default.inc.php
@@ -0,0 +1,43 @@
+<style>
+
+header {
+  text-align: right;
+  background: #ceeae8;
+  padding: 20px 20px;
+}
+
+nav ul li {
+  display: inline;
+}
+
+nav a {
+  text-decoration: none;
+  color: #231f20;
+  font-family: "Helvetica", Arial, sans-serif;
+  margin: 5px;
+  padding: 5px;
+}
+
+.container {
+  max-width: 900px;
+  margin: 50px auto 180px;
+  text-align: center;
+}
+
+
+</style>
+
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="contact.html">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+<div class="container".
+  <h1>Sorry something went wrong with your submission</h1>
+  <p>Please try again </p>
+</div>
